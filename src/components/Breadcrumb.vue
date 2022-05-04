1a49f5b388e8867b0e00485567a881c3481299d2
Forevka <32968153+Forevka@users.noreply.github.com>
2022-05-04 19:13:51 +0000

    up

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -15,4 +15,53 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+.breadcrumb {
+  --breadcrumb-divider: "/";
+  --breadcrumb-divider-color: var(--secondary-color-darker);
+  --breadcrumb-color: black;
+  --breadcrumb-active-color: var(--primary-color);
+  display: flex;
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+.breadcrumb-item a {
+  position: relative;
+  text-decoration: none;
+}
+
+.breadcrumb-item a::before {
+  position: absolute;
+  content: "";
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 0.0625rem;
+  background: currentColor;
+  transform: scaleX(0);
+  transition: 0.3s;
+}
+
+.breadcrumb-item a:hover::before {
+  transform: scaleX(1);
+}
+
+.breadcrumb-item:not(:first-child) {
+  padding-left: 0.5625rem;
+}
+
+.breadcrumb-item:not(:first-child)::before {
+  content: var(--breadcrumb-divider);
+  padding-right: 0.5625rem;
+  color: var(--breadcrumb-divider-color);
+}
+
+.breadcrumb-item:not(:last-child) a {
+  color: var(--breadcrumb-color);
+}
+
+.breadcrumb-item:last-child a {
+  color: var(--breadcrumb-active-color);
+}
 </style>
